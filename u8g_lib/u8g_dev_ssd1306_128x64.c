4b7e214a269f28e8f869a4f319fa879f67e9048e
puriso <nrs.yuuki@gmail.com>
2019-08-19 09:35:00 +0900

    fix a gap

--- u8g_lib/u8g_dev_ssd1306_128x64.c
+++ u8g_lib/u8g_dev_ssd1306_128x64.c
@@ -183,7 +183,7 @@ static const uint8_t u8g_dev_ssd1306_128x64_data_start[] PROGMEM = {
   U8G_ESC_ADR(0),           /* instruction mode */
   U8G_ESC_CS(1),             /* enable chip */
   0x010,		/* set upper 4 bit of the col adr to 0 */
-  0x000,		/* set lower 4 bit of the col adr to 0  */
+  0x002,		/* set lower 4 bit of the col adr to 0  */
   U8G_ESC_END                /* end of sequence */
 };
 
